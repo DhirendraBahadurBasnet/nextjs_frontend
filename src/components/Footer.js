94a136c139786a3dd6754d43dc3da771f3bccfb3
DhirendraBahadurBasnet <DhirendraBdrBasnet@gmail.com>
2024-08-10 22:00:11 +0545

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-blakish text-gray-500 text-center py-4 pb-16 md:pb-4'>
+      Copyright &copy; <br/> All Rights Reserved 2024
+    </div>
+  )
+}
+
+export default Footer
